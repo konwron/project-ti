3019e05d35139bdc9c14d5d679b634ada104d96d
konwron <kondziow1997@gmail.com>
2022-05-27 20:59:53 +0200

    project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import Shop from './pages/Shop';
+import Contact from './pages/Contact';
+import ErrorPage from './pages/ErrorPage';
+import './pages/scss/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<Router>
+			<div className="App">
+				<header class="container-wrapper">
+					<div class="container">
+						<a href={HomePage}>
+							<img id="company-logo" src="assets/logo.png" alt="Logo" />
+						</a>
+						<nav>
+							<ul id="navigation">
+								<li><a href={HomePage}>Home</a></li>
+								<li><a href={Shop}>Shop</a></li>
+								<li><a href={Contact}>Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+				</header>
+				<Switch>
+					<Route path="/" component={HomePage} />
+					<Route path="/shop" component={Shop} />
+					<Route path="/contact" component={Contact} />
+					<Route path="*" component={ErrorPage} />
+				</Switch>
+				<section class="container-wrapper">
+					<div class="container"></div>
+				</section>
+				<footer class="container-wrapper">
+					<div class="container"></div>
+				</footer>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,7 @@
+const ErrorPage = () => {
+	return (
+		<div style={{ textAlign: "center" }}>
+			<h1>404: Page Not Found</h1>
+		</div>
+	);
+};
